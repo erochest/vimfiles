4f3655bb295bbb1198d5ea1dce126d2fd033aaaf
Eric Rochester <eric.rochester@willowtreeapps.com>
2023-07-14 17:50:36 -0400

    started adding config

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
 
-" Nothing to see here. Move along, citizen.
+-- Nothing to see here. Move along, citizen.
+
+vim.opt.backup = true
+vim.opt.backupskip = ["/tmp/*", "/private/tmp/*"]
+vim.opt.backspace = "indent,eol,start"
+vim.opt.hlsearch = true
+vim.opt.joinspaces = false
+vim.opt.modeline = true
+vim.opt.foldmethod = "marker"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wildignore= "*.swp,*.bak,*.pyc,*.ho,*.hi,*.o"
+-- vim.opt.pastetoggle=<F2>
+vim.opt.cursorline = true
+vim.opt.wildmenu = true
 
