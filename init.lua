49934e0ad329b93cff846aaadeb2602e88d66462
Eric Rochester <erochest@gmail.com>
2023-07-21 07:20:38 -0400

    use lua tables instead of strings

--- init.lua
+++ init.lua
@@ -2,15 +2,15 @@
 -- Nothing to see here. Move along, citizen.
 
 vim.opt.backup = true
-vim.opt.backupskip = "/tmp/*,/private/tmp/*"
-vim.opt.backspace = "indent,eol,start"
+vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
+vim.opt.backspace = { indent = true, eol = true, start=true }
 vim.opt.hlsearch = true
 vim.opt.joinspaces = false
 vim.opt.modeline = true
 vim.opt.foldmethod = "marker"
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.wildignore= "*.swp,*.bak,*.pyc,*.ho,*.hi,*.o"
+vim.opt.wildignore= { "*.swp", "*.bak", "*.pyc", "*.ho", "*.hi", "*.o"}
 -- vim.opt.pastetoggle=<F2>
 vim.opt.cursorline = true
 vim.opt.wildmenu = true
@@ -23,3 +23,14 @@ vim.keymap.set("n", "Q", "gqap")
 vim.keymap.set("n", "<silent><leader>/", ":nohlsearch<CR>")
 vim.keymap.set("n", "<silent><leader>S", ":set nospell<CR>")
 
+vim.keymap.set("n", "<C-n>", ":next<CR>")
+
+-- function s:ChCwd()
+-- 	execute "chdir " . escape(expand("%:p:h"), ' ')
+-- endfunction
+-- com! ChCwd call s:ChCwd()
+--
+-- function s:StripWS()
+-- 	%s/\s\+$//ge
+-- endfunction
+-- com! StripWS call s:StripWS()
