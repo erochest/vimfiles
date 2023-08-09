64313ec70cb9bca2e2723653ff286f2993771f55
Eric Rochester <erochest@gmail.com>
2023-08-09 07:13:39 -0400

    cmp

--- init.lua
+++ init.lua
@@ -242,30 +242,34 @@ require("lazy").setup({
 		},
 	},
 
-	-- {
-	-- 	"hrsh7th/nvim-cmp",
-	-- 	dependencies = {
-	-- 		"hrsh7th/cmp-nvim-lsp",
-	-- 		"hrsh7th/cmp-nvim-lua",
-	-- 		"hrsh7th/cmp-buffer",
-	-- 		"hrsh7th/cmp-path",
-	-- 		-- "hrsh7th/cmp-cmdline",
-	-- 		-- "saadparwaiz1/cmp_luasnip",
-	-- 		-- "L3MON4D3/LuaSnip",
-	-- 	},
-	-- 	opts = function(cmp, opts)
-	-- 		print(cmp)
-	-- 		print(opts)
-	-- 		opts.sources = cmp.config.sources({
-	-- 				{ name = "nvim_lsp" },
-	-- 				{ name = "nvim_lua" },
-	-- 			}, {
-	-- 				{ name = "buffer" },
-	-- 				{ name = "path" },
-	-- 			})
-	-- 		return opts
-	-- 	end,
-	-- },
+	{ "hrsh7th/cmp-nvim-lsp", },
+	{ "hrsh7th/cmp-nvim-lua", },
+	{ "hrsh7th/cmp-buffer", },
+	{ "hrsh7th/cmp-path", },
+	{ "hrsh7th/cmp-cmdline", },
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline"
+			-- 		-- "saadparwaiz1/cmp_luasnip",
+			-- 		-- "L3MON4D3/LuaSnip",
+		},
+		opts = function(_, opts)
+			opts.sources = require("cmp").config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "nvim_lua" },
+			}, {
+				{ name = "buffer" },
+				{ name = "path" },
+			})
+			return opts
+		end,
+	},
 
 })
 
