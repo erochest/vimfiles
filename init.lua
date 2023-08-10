ef1ec0841d44919d46a73d4d72cf6c8532ec1308
Eric Rochester <eric.rochester@willowtreeapps.com>
2023-08-10 12:56:14 -0400

    formatter

--- init.lua
+++ init.lua
@@ -368,6 +368,71 @@ require("lazy").setup({
 			})
 		end,
 	},
+
+	{
+		"mhartington/formatter.nvim",
+		keys = {
+			{
+				"<leader>fm",
+				"<cmd>Format<CR>",
+				mode = "n",
+				desc = "Format the current buffer",
+			},
+			{
+				"<leader>fM",
+				"<cmd>FormatWrite<CR>",
+				mode = "n",
+				desc = "Format the current buffer and write it to disk",
+			},
+		},
+		cmd = {
+			"Format",
+			"FormatWrite",
+			"FormatLock",
+			"FormatWriteLock",
+		},
+		event = {
+			"BufWritePre",
+		},
+		config = function()
+			formatter = require("formatter")
+			formatter.setup({
+				logging = false,
+				filetype = {
+					["*"] = {
+						-- "formatter.filetypes.any" defines default configurations for any
+						-- filetype
+						require("formatter.filetypes.any").remove_trailing_whitespace
+					},
+					["json"] = {
+						require("formatter.filetypes.json").prettier,
+					},
+					["lua"] = {
+						require("formatter.filetypes.lua").lua_format,
+					},
+					["markdown"] = {
+						require("formatter.filetypes.markdown").prettier,
+					},
+					["python"] = {
+						require("formatter.filetypes.python").black,
+						require("formatter.filetypes.python").isort,
+					},
+					["rust"] = {
+						require("formatter.filetypes.rust").rustfmt,
+					},
+					["toml"] = {
+						require("formatter.filetypes.toml").prettier,
+					},
+					["yaml"] = {
+						require("formatter.filetypes.yaml").prettier,
+					},
+				},
+			})
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				command = "FormatWrite",
+			})
+		end,
+	},
 })
 
 -- DONE: nvim-cmp https://vonheikemen.github.io/devlog/tools/setup-nvim-lspconfig-plus-nvim-cmp/
@@ -375,8 +440,8 @@ require("lazy").setup({
 -- DONE: nvim-dap https://microsoft.github.io/debug-adapter-protocol/implementors/adapters/
 -- DONE: nvim-dap-ui https://github.com/rcarriga/nvim-dap-ui#usage
 -- DONE: snippets https://github.com/hrsh7th/nvim-cmp#recommended-configuration
--- TODO: linters https://github.com/williamboman/mason.nvim#how-to-use-installed-packages
--- TODO: formatters https://github.com/williamboman/mason.nvim#how-to-use-installed-packages
+-- DONE: linters https://github.com/williamboman/mason.nvim#how-to-use-installed-packages
+-- DONE: formatters https://github.com/williamboman/mason.nvim#how-to-use-installed-packages
 -- TODO: telescope-undo
 -- TODO: copilot-cmp
 -- TODO: statusline
