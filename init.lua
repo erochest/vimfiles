0b2751f22ff8cc609b1b9d0cb25b24eb859ea1c2
Eric Rochester <erochest@gmail.com>
2023-08-10 07:33:15 -0400

    nvim-lint

--- init.lua
+++ init.lua
@@ -349,6 +349,25 @@ require("lazy").setup({
 			}
 		end,
 	},
+
+	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			lint = require("lint")
+			lint.linters_by_ft = {
+				json = { "jsonlint", },
+				lua = { "luacheck", },
+				markdown = { "vale", },
+				python = { "flake8", "mypy", "pylint" },
+				yaml = { "yamllint", },
+			}
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				callback = function()
+					require("lint").try_lint()
+				end,
+			})
+		end,
+	},
 })
 
 -- DONE: nvim-cmp https://vonheikemen.github.io/devlog/tools/setup-nvim-lspconfig-plus-nvim-cmp/
