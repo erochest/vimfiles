5f6292a4dc533c842f436c9740fc50a06a29179c
Eric Rochester <erochest@gmail.com>
2023-08-15 06:58:30 -0400

    changed todotxt plugin

--- lua/plugins/org.lua
+++ lua/plugins/org.lua
@@ -1,34 +1,38 @@
+-- these are copied from the todo.txt plugin
+-- https://gitlab.com/dbeniamine/todo.txt-vim/-/blob/master/ftdetect/todo.vim?ref_type=heads
+vim.cmd([[
+autocmd BufNewFile,BufRead [Tt]odo.txt set filetype=todo
+autocmd BufNewFile,BufRead [Tt]odo-\d\\\{4\}-\d\\\{2\}-\d\\\{2\}.txt set filetype=todo
+autocmd BufNewFile,BufRead [Tt]odo-\d\\\{4\}-\d\\\{2\}.txt set filetype=todo
+autocmd BufNewFile,BufRead \d\\\{4\}-\d\\\{2\}-\d\\\{2\}-[Tt]odo.txt set filetype=todo
+autocmd BufNewFile,BufRead \d\\\{4\}-\d\\\{2\}-[Tt]odo.txt set filetype=todo
+autocmd BufNewFile,BufRead [Tt]oday.txt set filetype=todo
+autocmd BufNewFile,BufRead [Dd]one.txt set filetype=todo
+autocmd BufNewFile,BufRead [Dd]one-\d\\\{4\}-\d\\\{2\}-\d\\\{2\}.txt set filetype=todo
+autocmd BufNewFile,BufRead [Dd]one-\d\\\{4\}-\d\\\{2\}.txt set filetype=todo
+autocmd BufNewFile,BufRead \d\\\{4\}-\d\\\{2\}-\d\\\{2\}-[Dd]one.txt set filetype=todo
+autocmd BufNewFile,BufRead \d\\\{4\}-\d\\\{2\}-[Dd]one.txt set filetype=todo
+autocmd BufNewFile,BufRead [Dd]one-[Tt]oday.txt set filetype=todo
+autocmd BufNewFile,BufRead */.todo/*.txt set filetype=todo
+
+if exists("$TODO_DIR")
+    au BufNewFile,BufRead $TODO_DIR/*.txt set filetype=todo
+endif
+
+if exists("$TODO_FILE")
+    au BufNewFile,BufRead $TODO_FILE set filetype=todo
+endif
+
+if exists("$DONE_FILE")
+    au BufNewFile,BufRead $DONE_FILE set filetype=todo
+endif
+]])
+
 return {
 	{
-		'arnarg/todotxt.nvim',
-		dependencies = { 'MunifTanjim/nui.nvim', },
-		cmd = {
-			"ToDoTxtCapture",
-			"ToDoTxtTasksToggle",
-		},
-		keys = {
-			{ "<leader>tt", "<cmd>ToDoTxtTasksToggle<cr>", desc = "Toggle the todo list", mode = "n", },
-			{ "<leader>tc", "<cmd>ToDoTxtCapture<cr>", desc = "Capture a new todo", mode = "n", },
-		},
-		config = function()
-			local filenames = {
-				"./todo.txt",
-				"~/todo.txt",
-				"~/.todo.txt",
-				"~/.todo/todo.txt",
-				"~/Dropbox/todo.txt",
-			}
-			local todo_file = nil
-			for _, filename in ipairs(filenames) do
-				if vim.fn.filereadable(filename) == 1 then
-					todo_file = filename
-					break
-				end
-			end
-			require('todotxt-nvim').setup({
-				todo_file = todo_file,
-			})
-		end,
+		"dbeniamine/todo.txt-vim",
+		url = "https://github.com/dbeniamine/todo.txt-vim.git",
+		ft = { "todo", },
 	},
 
 }
