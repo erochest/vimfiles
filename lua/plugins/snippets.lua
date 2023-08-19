7514ea5783fb31824a7890783a047231afe96990
Eric Rochester <erochest@gmail.com>
2023-08-18 20:40:06 -0400

    use friendly-snippets

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -2,6 +2,7 @@ return {
      {
         "L3MON4D3/LuaSnip",
         version = "2.*",
+        dependencies = { "rafamadriz/friendly-snippets" },
         keys = {
             { "<C-K>", function() require("luasnip").expand() end, mode = {"i"}, silent = true, desc = "Expand snippet" },
             { "<C-L>", function() require("luasnip").jump( 1) end, mode = {"i", "s"}, silent = true, desc = "Next snippet expansion" },
@@ -20,7 +21,8 @@ return {
         },
         config = function()
             require("luasnip").setup({})
-            require("luasnip.loaders.from_snipmate").lazy_load()
+            require("luasnip.loaders.from_vscode").lazy_load()
         end,
     },
+    "rafamadriz/friendly-snippets",
 }
