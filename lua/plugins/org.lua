8ea82f70cc143257bc8f2fc83884639cebf521af
Eric Rochester <erochest@gmail.com>
2023-08-19 17:28:04 -0400

    neorg

--- lua/plugins/org.lua
+++ lua/plugins/org.lua
@@ -5,4 +5,24 @@ return {
         ft = { "todo", },
     },
 
+    {
+        "nvim-neorg/neorg",
+        build = ":Neorg sync-parsers",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("neorg").setup({
+                load = {
+                    ["core.defaults"] = {}, -- Loads default behaviour
+                    ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                    ["core.dirman"] = { -- Manages Neorg workspaces
+                        config = {
+                            workspaces = {
+                                notes = "~/Dropbox/notes",
+                            },
+                        },
+                    },
+                },
+            })
+        end,
+    },
 }
