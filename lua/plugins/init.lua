e69a298a083e7350f0093078a3ef5bee1a183e79
Eric Rochester <erochest@gmail.com>
2023-08-20 15:44:18 -0400

    hop

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -30,4 +30,26 @@ return {
         },
     },
 
+    {
+        "phaazon/hop.nvim",
+        config = function()
+            local hop = require("hop")
+            hop.setup()
+
+            -- More keymaps based on https://github.com/phaazon/hop.nvim/wiki/Commands
+            local directions = require('hop.hint').HintDirection
+            vim.keymap.set('', 'f', function()
+                hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+            end, {remap=true, desc="Hop to next character"})
+            vim.keymap.set('', 'F', function()
+                hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+            end, {remap=true, desc="Hop to previous character"})
+            vim.keymap.set('', 't', function()
+                hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+            end, {remap=true, desc="Hop to next character after cursor"})
+            vim.keymap.set('', 'T', function()
+                hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+            end, {remap=true, desc="Hop to previous character after cursor"})
+        end,
+    },
 }
