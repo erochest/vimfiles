24290b359e1751e514f3d696e6d273d682eb3b5f
Eric Rochester <erochest@gmail.com>
2023-08-21 07:23:06 -0400

    Retabbed files

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,55 +1,55 @@
 return {
-    'nvim-lua/plenary.nvim',
-    "nvim-tree/nvim-web-devicons",
-    'MunifTanjim/nui.nvim',
+  'nvim-lua/plenary.nvim',
+  "nvim-tree/nvim-web-devicons",
+  'MunifTanjim/nui.nvim',
 
-    {
-        "jedrzejboczar/possession.nvim",
-        dependencies = { "nvim-lua/plenary.nvim", },
-        cmd = {
-            "PossessionSave",
-            "PossessionLoad",
-            "PossessionRename",
-            "PossessionClose",
-            "PossessionDelete",
-            "PossessionShow",
-            "PossessionList",
-            "PossessionMigrate",
-        },
-        opts = {
-            commands = {
-                save = 'PossessionSave',
-                load = 'PossessionLoad',
-                rename = 'PossessionRename',
-                close = 'PossessionClose',
-                delete = 'PossessionDelete',
-                show = 'PossessionShow',
-                list = 'PossessionList',
-                migrate = 'PossessionMigrate',
-            },
-        },
+  {
+    "jedrzejboczar/possession.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", },
+    cmd = {
+      "PossessionSave",
+      "PossessionLoad",
+      "PossessionRename",
+      "PossessionClose",
+      "PossessionDelete",
+      "PossessionShow",
+      "PossessionList",
+      "PossessionMigrate",
     },
-
-    {
-        "phaazon/hop.nvim",
-        config = function()
-            local hop = require("hop")
-            hop.setup()
-
-            -- More keymaps based on https://github.com/phaazon/hop.nvim/wiki/Commands
-            local directions = require('hop.hint').HintDirection
-            vim.keymap.set('', 'f', function()
-                hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-            end, {remap=true, desc="Hop to next character"})
-            vim.keymap.set('', 'F', function()
-                hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-            end, {remap=true, desc="Hop to previous character"})
-            vim.keymap.set('', 't', function()
-                hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-            end, {remap=true, desc="Hop to next character after cursor"})
-            vim.keymap.set('', 'T', function()
-                hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-            end, {remap=true, desc="Hop to previous character after cursor"})
-        end,
+    opts = {
+      commands = {
+        save = 'PossessionSave',
+        load = 'PossessionLoad',
+        rename = 'PossessionRename',
+        close = 'PossessionClose',
+        delete = 'PossessionDelete',
+        show = 'PossessionShow',
+        list = 'PossessionList',
+        migrate = 'PossessionMigrate',
+      },
     },
+  },
+
+  {
+    "phaazon/hop.nvim",
+    config = function()
+      local hop = require("hop")
+      hop.setup()
+
+      -- More keymaps based on https://github.com/phaazon/hop.nvim/wiki/Commands
+      local directions = require('hop.hint').HintDirection
+      vim.keymap.set('', 'f', function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+      end, {remap=true, desc="Hop to next character"})
+      vim.keymap.set('', 'F', function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+      end, {remap=true, desc="Hop to previous character"})
+      vim.keymap.set('', 't', function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+      end, {remap=true, desc="Hop to next character after cursor"})
+      vim.keymap.set('', 'T', function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+      end, {remap=true, desc="Hop to previous character after cursor"})
+    end,
+  },
 }
--- lua/plugins/nav.lua
+++ lua/plugins/nav.lua
@@ -1,83 +1,83 @@
 return {
-    {
-        "nvim-tree/nvim-tree.lua",
-        cmd = {
-            "NvimTreeToggle",
-            "NvimTreeOpen",
-            "NvimTreeClose",
-            "NvimTreeRefresh",
-            "NvimTreeFindFile",
-        },
-        keys = {
-            { "<leader>nt", "<cmd>NvimTreeToggle<CR>", mode = "n", desc = "Toggle the file tree", },
-            { "<leader>nf", "<cmd>NvimTreeFindFile<CR>", mode = "n", desc = "Find a file in the file tree", },
-        },
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-        config = true,
+  {
+    "nvim-tree/nvim-tree.lua",
+    cmd = {
+      "NvimTreeToggle",
+      "NvimTreeOpen",
+      "NvimTreeClose",
+      "NvimTreeRefresh",
+      "NvimTreeFindFile",
     },
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-        end,
-        config = function()
-            local wk = require("which-key")
-            wk.setup({})
-            wk.register({
-                ["<leader>c"] = { name = "Code" },
-                ["<leader>C"] = { name = "Clipboard" },
-                ["<leader>d"] = { name = "Code Diagnostic" },
-                ["<leader>f"] = { name = "Find" },
-                ["<leader>F"] = { name = "Format" },
-                ["<leader>g"] = { name = "Go" },
-                ["<leader>l"] = { name = "LSP" },
-                ["<leader>n"] = { name = "Navigation File Tree" },
-                ["<leader>w"] = { name = "Workspace" },
-                ["<leader>z"] = { name = "Folds" },
-            })
-        end,
+    keys = {
+      { "<leader>nt", "<cmd>NvimTreeToggle<CR>", mode = "n", desc = "Toggle the file tree", },
+      { "<leader>nf", "<cmd>NvimTreeFindFile<CR>", mode = "n", desc = "Find a file in the file tree", },
     },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = true,
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    config = function()
+      local wk = require("which-key")
+      wk.setup({})
+      wk.register({
+        ["<leader>c"] = { name = "Code" },
+        ["<leader>C"] = { name = "Clipboard" },
+        ["<leader>d"] = { name = "Code Diagnostic" },
+        ["<leader>f"] = { name = "Find" },
+        ["<leader>F"] = { name = "Format" },
+        ["<leader>g"] = { name = "Go" },
+        ["<leader>l"] = { name = "LSP" },
+        ["<leader>n"] = { name = "Navigation File Tree" },
+        ["<leader>w"] = { name = "Workspace" },
+        ["<leader>z"] = { name = "Folds" },
+      })
+    end,
+  },
 
-    {
-        'stevearc/aerial.nvim',
-        config = function()
-            require("aerial").setup({})
-            vim.keymap.set('n', '<leader>ct', '<cmd>AerialToggle!<CR>', {desc = "Toggle Aerial"})
-        end,
-        -- Optional dependencies
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-tree/nvim-web-devicons"
-        },
-        on_attach = function(bufnr)
-            vim.keymap.set("n", "<leader>g{", "<cmd>AerialPrev<cr>", {
-                buffer = bufnr,
-                desc = "Previous Aerial node",
-            })
-            vim.keymap.set("n", "<leader>g}", "<cmd>AerialNext<cr>", {
-                buffer = bufnr,
-                desc = "Next Aerial node",
-            })
-        end,
+  {
+    'stevearc/aerial.nvim',
+    config = function()
+      require("aerial").setup({})
+      vim.keymap.set('n', '<leader>ct', '<cmd>AerialToggle!<CR>', {desc = "Toggle Aerial"})
+    end,
+    -- Optional dependencies
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
     },
+    on_attach = function(bufnr)
+      vim.keymap.set("n", "<leader>g{", "<cmd>AerialPrev<cr>", {
+        buffer = bufnr,
+        desc = "Previous Aerial node",
+      })
+      vim.keymap.set("n", "<leader>g}", "<cmd>AerialNext<cr>", {
+        buffer = bufnr,
+        desc = "Next Aerial node",
+      })
+    end,
+  },
 
-    {
-        "SmiteshP/nvim-navic",
-        config = function()
-            require("nvim-navic").setup({})
-            vim.api.nvim_create_autocmd({"LspAttach"}, {
-                pattern = "*",
-                callback = function(args)
-                    -- print("args: ", vim.inspect(args))
-                    local bufnr = args.buf
-                    local client = vim.lsp.get_client_by_id(args.data.client_id)
-                    require("nvim-navic").attach(client, bufnr)
-                end,
-            })
+  {
+    "SmiteshP/nvim-navic",
+    config = function()
+      require("nvim-navic").setup({})
+      vim.api.nvim_create_autocmd({"LspAttach"}, {
+        pattern = "*",
+        callback = function(args)
+          -- print("args: ", vim.inspect(args))
+          local bufnr = args.buf
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          require("nvim-navic").attach(client, bufnr)
         end,
-    },
+      })
+    end,
+  },
 }
