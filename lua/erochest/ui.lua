494374c33a9280cb19824ab65c2a6af45917eead
Eric Rochester <eric.rochester@willowtreeapps.com>
2023-08-23 17:39:52 -0400

    Added testing resources

--- lua/erochest/ui.lua
+++ lua/erochest/ui.lua
@@ -1,6 +1,10 @@
 
 local M = {}
 
+-- resources:
+-- https://blog.insiderattack.net/the-journeymans-guide-to-lua-unit-testing-41642825314a
+-- https://lunarmodules.github.io/busted/
+-- https://medium.com/@ioverthoughtthis/a-lua-testing-framework-in-31-lines-2f6cfd067126
 function M.get_git_branch()
 end
 
