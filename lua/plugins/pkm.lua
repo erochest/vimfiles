7aa53dfdd6eeb66c76ccc3ff2141cdb03427428b
Eric Rochester <eric.rochester@willowtreeapps.com>
2023-08-25 17:03:39 -0400

    More resources and a default vault for Obsidian plugin

--- lua/plugins/pkm.lua
+++ lua/plugins/pkm.lua
@@ -1,6 +1,22 @@
 
 -- https://github.com/epwalsh/obsidian.nvim
 -- TODO: sniff out all my vaults here, or set them from an env var
+--
+-- More resources
+-- https://github.com/arakkkkk/kanban.nvim
+-- https://lib.rs/crates/rust-kanban
+-- https://neovimcraft.com/plugin/nvim-neorg/neorg/index.html
+-- https://news.ycombinator.com/item?id=17547278
+-- https://www.reddit.com/r/neovim/comments/tvej9t/kabmat_a_tui_program_for_managing_kanban_boards/
+-- https://github.com/PlankCipher/kabmat
+-- https://github.com/epwalsh/obsidian.nvim/issues
+-- https://github.com/epwalsh/obsidian.nvim/issues/170
+-- https://github.com/epwalsh/obsidian.nvim/issues/128
+-- https://github.com/epwalsh/obsidian.nvim/issues/126
+-- https://github.com/epwalsh/obsidian.nvim/issues/115
+-- https://github.com/epwalsh/obsidian.nvim/issues/88
+
+local vault = vim.fn.expand("~/Dropbox/g")
 
 return {
   {
@@ -16,7 +32,7 @@ return {
       "nvim-telescope/telescope.nvim",
     },
     opts = {
-      -- dir = "~/my-vault",  -- no need to call 'vim.fn.expand' here
+      dir = vault,
 
       -- see below for full list of options 👇
     },
