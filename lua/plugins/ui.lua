544b9fbe2cb5a37fea990e9875e8d15078cfa3f1
Eric Rochester <erochest@gmail.com>
2023-08-25 07:17:46 -0400

    reformatted file and added indent-blankline options

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,50 +2,57 @@
 -- local branch_ticket = require('erochest.ui').branch_ticket
 
 return {
-    { "shaunsingh/nord.nvim", lazy = true, },
-    {
-        "marko-cerovac/material.nvim",
-        lazy = true,
-        setup = {
-            plugins = {
-                "dap",
-                -- "dashboard",
-                "gitsigns",
-                "hop",
-                "indent-blankline",
-                -- "lspsaga",
-                -- "mini",
-                "neogit",
-                -- "neorg",
-                "nvim-cmp",
-                "nvim-navic",
-                "nvim-tree",
-                "nvim-web-devicons",
-                -- "sneak",
-                "telescope",
-                "trouble",
-                "which-key",
-            },
-        },
+  { "shaunsingh/nord.nvim", lazy = true, },
+  {
+    "marko-cerovac/material.nvim",
+    lazy = true,
+    setup = {
+      plugins = {
+        "dap",
+        -- "dashboard",
+        "gitsigns",
+        "hop",
+        "indent-blankline",
+        -- "lspsaga",
+        -- "mini",
+        "neogit",
+        -- "neorg",
+        "nvim-cmp",
+        "nvim-navic",
+        "nvim-tree",
+        "nvim-web-devicons",
+        -- "sneak",
+        "telescope",
+        "trouble",
+        "which-key",
+      },
     },
+  },
 
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-            "SmiteshP/nvim-navic",
-        },
-        opts = {
-            sections = {
-                -- lualine_b = {'branch', 'diff', 'diagnostics'},
-                -- lualine_b = {branch_ticket, 'diff', 'diagnostics'},
-                lualine_c = {'navic'},
-            },
-        },
-        lazy = false,
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "SmiteshP/nvim-navic",
     },
+    opts = {
+      sections = {
+        -- lualine_b = {'branch', 'diff', 'diagnostics'},
+        -- lualine_b = {branch_ticket, 'diff', 'diagnostics'},
+        lualine_c = {'navic'},
+      },
+    },
+    lazy = false,
+  },
 
-  { "lukas-reineke/indent-blankline.nvim", },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    opts = {
+      space_char_blankline = " ",
+      show_current_context = true,
+      show_current_context_start = true,
+    },
+  },
 
   { "rcarriga/nvim-notify", },
 
